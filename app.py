371a41c8b386ff7577a539e48531786c1612942c
ArkadiuszZabicki <arkadiusz.zabicki@gmail.com>
2023-08-06 18:00:02 +0200

    Fixed app.py to run the app; fixed return for app

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p1>Hello world</p>'
+    return '<p>Hello world</p>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
